dc06088333f47d8d7082781152576c279b46bb72
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2016-12-03 18:00:39 +0200

    shift

--- app/controllers/cats_controller.php
+++ app/controllers/cats_controller.php
@@ -2,7 +2,7 @@
  * @author Author "Mohamed Elsayed"  
  * @author Author Email "me@mohamedelsayed.net"
  * @link http://www.mohamedelsayed.net
- * @copyright Copyright (c) 2015 Programming by "mohamedelsayed.net"
+ * @copyright Copyright (c) 2017 Programming by "mohamedelsayed.net"
  */
 require_once AUTH_CONTROLLER_PATH;
 class CatsController extends AuthController {
